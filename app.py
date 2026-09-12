55fa9bcf4c3d84b63d4c784beae028e0650068a5
Fabricio <fabriciourrutia4@gmail.com>
2026-09-12 14:30:21 -0500

    Limpiar marcadores de conflicto

--- app.py
+++ app.py
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
-'def reserva():
-    return "Reserva confirmada en el laboratorio"'
-
-=======
 def reserva():
-    return "Hay 15 PCs disponibles"
->>>>>>> origin/feature-disponibilidad
+    return "Hay 15 PCs disponibles. Reserva confirmada en el laboratorio"
